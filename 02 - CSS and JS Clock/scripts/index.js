2f7345ff26cce8d5368f676a9abecde50c46a125
Atharv <atharvakumar0@gmail.com>
2024-03-19 01:04:38 +0530

    Added second project

---
+++ 02 - CSS and JS Clock/scripts/index.js
@@ -0,0 +1,21 @@
+const hourHand = document.querySelector('.hour-hand');
+const minuteHand = document.querySelector('.minute-hand');
+const secondHand = document.querySelector('.second-hand');
+
+function setDate() {
+    const now = new Date();
+
+    const hour = now.getHours();
+    const hourDegrees = (hour / 60) * 360 + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+    
+    const minute = now.getMinutes();
+    const minuteDegrees = (minute / 60) * 360 + 90;
+    minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
+    
+    const second = now.getSeconds();
+    const secondDegrees = (second / 60) * 360 + 90;
+    secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
